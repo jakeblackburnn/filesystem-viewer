5837a3cb7d1b0c32fca8a260c5939fe317b6def7
John Blackburn <jackblackburn@Johns-MacBook-Pro.local>
2025-01-07 14:25:19 -0500

    Move file loading to separate function

--- script.js
+++ script.js
@@ -2,26 +2,33 @@
 // Created by J. Blackburn - Dec 19 2024
 //
 
-fetch('/file.txt').then(response => {
+function loadFileContentIntoElement(filepath, elementId) {
 
-    if (!response.ok) {     // check that file is served correctly
+	fetch(filepath).then(response => {
 
-      throw new Error('Network response was not ok');
+	    if (!response.ok) {     // check that file is served correctly
 
-    }
+	      throw new Error('Network response was not ok');
+	    }
 
 
-    return response.text(); // Convert the response to text
+	    return response.text(); // Convert the response to text
 
-  }).then(data => {         // then load the text into element
+	  }).then(data => {         // then load the text into element
 
-    document.getElementById('root').textContent = data;
+	    document.getElementById(elementId).textContent = data;
 
-  })
+	  })
 
-  .catch(error => {         // or load error message instead
+	  .catch(error => {         // or load error message instead
 
-    console.error('Error fetching the file:', error);
-    document.getElementById('root').textContent = 'Error loading file content';
+	    console.error('Error fetching the file:', error);
+	    document.getElementById('root').textContent = 'Error loading file content';
 
-  });
+	  });
+}
+
+
+
+
+loadFileContentIntoElement('/file.txt', 'root');
