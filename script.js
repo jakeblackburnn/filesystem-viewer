6bcb2e50fe79ae0f7881b4e6602532944764f2fd
John Blackburn <jackblackburnn@icloud.com>
2025-03-11 18:33:55 -0400

    Add render filesystem tree

--- script.js
+++ script.js
@@ -2,28 +2,48 @@
 // Created by J. Blackburn - Dec 19 2024
 //
 
+function renderFilesystemTree(tree, parentPath="") {
+	const ul = document.createElement('ul');
+
+	tree.forEach( item => {
+		const li = document.createElement('li');
+		const fullPath = parentPath ? `${parentPath}/${item.name}` : item.name;
+
+		if ( item.type === 'dir' ) {
+
+			const span = document.createElement('span');
+			span.textContent = item.name;
+
+			li.appendChild(span);
+			if ( item.children && item.children.length > 0 ) {
+				li.appendChild( renderFilesystemTree( item.children, fullPath) );
+			}
+
+		} else if ( item.type === 'file' ) {
+
+			const a = document.createElement('a');
+			a.href = 'fs/' + fullPath;
+			a.textContent = item.name;
+			
+			li.appendChild(a);
+
+		}
+		ul.appendChild(li);
+
+	}); // end for each
+
+	return ul;
+}
+
 function loadFileMap() {
 
 	const fileMapElement = document.getElementById('file-map');
 
 	fetch('/fs')
 		.then(response => response.json()) // convert to json
-		.then(files => {
+		.then(fsTree => {
 			fileMapElement.innerHTML = ''; // clear map contents
-			files.forEach(file => {
-
-				const a = document.createElement('a');
-				a.textContent = file;
-
-				const filepath = 'fs/' + file
-				a.href = filepath;
-
-				const li = document.createElement('li');
-				li.appendChild(a);
-
-				fileMapElement.appendChild(li);
-                    });
-
+			fileMapElement.appendChild( renderFilesystemTree(fsTree) );
 		});
 }
 
